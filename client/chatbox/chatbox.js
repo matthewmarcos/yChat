b73fdecfc60540ab35d04e73e8099dac338e7fc8
Matthew Marcos <matthewmarcos94@gmail.com>
2016-05-11 17:31:50 +0800

    Autoscrolls to bottom

--- client/chatbox/chatbox.js
+++ client/chatbox/chatbox.js
@@ -1,11 +1,25 @@
-import { Template } from 'meteor/templating';
 import { Messages } from '../../imports/collections/messages.js'
+import { Template } from 'meteor/templating';
+import { Tracker } from 'meteor/tracker';
 import './chatbox.html';
 
 Template.Chatbox.helpers({
     'message'() {
         return Messages.find();
     }
+});
+
+Template.Chatbox.onRendered(function() {
+
+    // Autoscrolls div to botton when a new class is added
+    Tracker.autorun(_ => {
+        let elem = $('.chatbox')[0];
+        const message = Messages.find().fetch();
+        if(message) {
+            elem.scrollTop = elem.scrollHeight;
+        }
+
+    });
 });
 
 Template.MessageTemplate.helpers({
